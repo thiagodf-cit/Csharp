a5e39275f5e40e83fb70377be721de05f885d653
martins86 <martins86@gmail.com>
2020-04-14 17:21:55 +0000

    Add class Department

---
+++ Enums/Entities/Department.cs
@@ -0,0 +1,14 @@
+namespace CourseEnums.Entities
+{
+    public class Department
+    {
+        public string Name { get; set; }
+
+        public Department () {}
+
+        public Department (string nome) 
+        {
+            Nome = nome;
+        }
+    }
+}
