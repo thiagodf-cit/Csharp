87c76482c570008a8f381a242702ea8cbee29e5e
thiagodf <thiagodf@ciandt.com>
2020-04-14 13:34:18 -0300

    init Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using CourseEnums.Entities.Enums;
+using CourseEnums.Entities;
+
+
+namespace CourseEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /////////////////// Enums //////////////////////////////////////////////////
+            ///
+
+            Order order = new Order
+            {
+                Id = 3082,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine("Pedido: " + order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Shipped");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+
+            ///
+            /////////////////// Enums //////////////////////////////////////////////////
+        }
+    }
+}
