4306a6f473ba89abb02a1d30e945d6234e85e4f7
martins86 <martins86@gmail.com>
2020-04-15 19:35:53 +0000

    Final Enums 1

--- Enums/Entities/Worker.cs
+++ Enums/Entities/Worker.cs
@@ -1,39 +1,49 @@
-using System;
-using System.Globalization;
 using System.Collections.Generic;
 using CourseEnums.Entities;
 using CourseEnums.Entities.Enums;
 
-namespace CourseEnuns.Entities
+namespace CourseEnums.Entities
 {
     public class Worker
     {
         public string Name { get; set; }
         public WorkerLevel Level { get; set; }
         public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
 
         public Worker() { }
 
-        public Worker(string name, WorkerLevel level, double baseSalary)
+        public Worker(string name, WorkerLevel level, double baseSalary, Department departmentName)
         {
-
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = departmentName;
+            Contracts = new List<HourContract>();
         }
 
         public void AddContract(HourContract contract)
         {
-
+            Contracts.Add(contract);
         }
 
         public void RemoveContract(HourContract contract)
         {
-
+            Contracts.Remove(contract);
         }
 
         public double Income(int year, int month)
         {
             double sum = BaseSalary;
-
-
+            foreach (HourContract contract in Contracts)
+            {
+                if (contract.Date.Year == year
+                    && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+            }
             return sum;
         }
     }
