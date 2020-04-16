72f8ffb64596b77c048212ad8ccfd176f683ad23
martins86 <martins86@gmail.com>
2020-04-16 20:06:26 +0000

    Create program

--- InfoProduct/Program.cs
+++ InfoProduct/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using System;
+using System.Globalization;
+using InfoProduct.Entities;
+using InfoProduct.Entities.Enums;
 
 namespace InfoProduct
 {
@@ -6,7 +9,43 @@ namespace InfoProduct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter cliente data:");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("\nEnter order data: ");
+            Console.Write("Status: ");
+            OrderStatus status = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Client client = new Client(name, email, birthDate);
+            Order order = new Order(DateTime.Now, status, client);
+
+            Console.Write("\nHow many items to this order? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Product product = new Product(productName, productPrice);
+
+                Console.Write("Quantity: ");
+                int productQuantity = int.Parse(Console.ReadLine());
+
+                OrderItem orderItem = new OrderItem(productQuantity, productPrice, productName);
+
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine("\nORDER SUMMARY:");
+            Console.WriteLine(order);
         }
     }
 }
