a64138bf1c5966cffe863d8c994a19469f78ad40
martins86 <martins86@gmail.com>
2020-04-16 20:07:51 +0000

    Create enum order status

---
+++ InfoProduct/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,10 @@
+namespace InfoProduct.Entities.Enums
+{
+    public enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
