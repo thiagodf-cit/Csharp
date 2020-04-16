00439d68feb1cec3b68d289b6413dd84a7383957
martins86 <martins86@gmail.com>
2020-04-16 14:50:16 +0000

    Create Comment

---
+++ Posts/Entities/Comment.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Posts.Entities
+{
+    public class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+        }
+        
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
