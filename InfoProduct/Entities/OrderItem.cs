4a08cc706dd70a06101ac4089cfc9e3e93ce0026
martins86 <martins86@gmail.com>
2020-04-16 20:07:06 +0000

    Create order item

---
+++ InfoProduct/Entities/OrderItem.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace InfoProduct.Entities
+{
+    public class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public List<Product> Product { get; set; } = new List<Product>();
+
+        public OrderItem()
+        {
+        }
+        public OrderItem(int quantity, double price)
+        {
+            Quantity = quantity;
+            Price = price;
+        }
+
+
+        public double SubTotal()
+        {
+            return Quantity * Price;
+        }
+    }
+}
