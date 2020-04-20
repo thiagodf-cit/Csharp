c578896ec142cafa65d5a743aff7f87005279441
Thiago Martins <martins86@gmail.com>
2020-04-20 15:21:02 -0300

    Create try and catch

---
+++ TryAndCatch/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryAndCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error! Format inválid " + e.Message);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Division by zero is not alloweb");
+            }
+        }
+    }
+}
