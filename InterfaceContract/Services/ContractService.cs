9e051db7470c774f0d2b8568190e86e3608d198d
Thiago Martins <martins86@gmail.com>
2020-04-24 16:40:41 -0300

    Create Interfaces

---
+++ InterfaceContract/Services/ContractService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using InterfaceContract.Entities;
+
+namespace InterfaceContract.Services
+{
+    class ContractService
+    {
+        private IOnlinePaymentService _onlinePaymentService;
+
+        public ContractService(IOnlinePaymentService onlinePaymentService)
+        {
+            _onlinePaymentService = onlinePaymentService;
+        }
+
+        public void ProcessContract(Contract contract, int months)
+        {
+            double basicQouta = contract.TotalValue / months;
+            for (int i = 1; i <= months; i++)
+            {
+                DateTime date = contract.Date.AddMonths(i);
+                double updateQuota = basicQouta + _onlinePaymentService.Interest(basicQouta, i);
+                double fullQuota = updateQuota + _onlinePaymentService.PaymentFee(updateQuota);
+                contract.AddInstallment(new Installment(date, fullQuota));
+            }
+        }
+    }
+}
