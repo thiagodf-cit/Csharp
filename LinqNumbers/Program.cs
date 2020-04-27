d75c088658b03855bbca1534b0d89dcea2f4ae95
Thiago Martins <martins86@gmail.com>
2020-04-27 15:22:11 -0300

    Create linq

---
+++ LinqNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            var result = numbers
+                .Where(n => n % 2 == 0)
+                .Select(n => n * 10);
+
+            foreach (int n in result) 
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
